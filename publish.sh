543355dc24659180876561f58b978092602342b7
Lorenzo Delana <lorenzo.delana@gmail.com>
2020-06-29 10:33:50 +0200

    create nupkg file script

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+exdir=$(dirname `readlink -f "$0"`)
+
+cd "$exdir"/Source/STL
+rm -fr bin
+dotnet pack -c Release STL-netcore.csproj
+#dotnet nuget push "$exdir"/Source/STL/bin/Release/*.nupkg -k $(cat ~/security/nuget-api.key) -s https://api.nuget.org/v3/index.json
+
+cd "$exdir"
